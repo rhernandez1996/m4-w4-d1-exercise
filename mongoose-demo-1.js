e522a254e39d8360cde06dd5715114f929df320f
rhernandez1996 <112348988+rhernandez1996@users.noreply.github.com>
2022-12-13 19:17:07 -0800

    push

---
+++ mongoose-demo-1.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:')); //will show error if the connection isnt showing if not it will open
+db.once('open', function() {
+  // we're connected!
+const fruitSchema = new mongoose.Schema({
+  name: String
+})
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+const apple = new Fruit({ name: 'apple' });
+console.log(apple.name); // 'apple'
+});
+
+
---
+++ mongoose-demo-2.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+kittySchema.methods.speak = function () { //speak method
+  const greeting = this.name
+  ? "Meow name is " + this.name
+  : "I don't have a name";
+  console.log(greeting);
+}
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+ 
+//create an Object
+const fluffy = new Kitten({ name: 'Fluffy' });
+fluffy.speak(); // "Meow name is Fluffy"
+});
+
+
